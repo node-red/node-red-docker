2ecc42d3733d2fda6c4268f142aec4bd7be25fa9
raymondmouthaan <raymondmmouthaan@gmail.com>
2018-05-04 21:28:24 +0200

    project cleanup

--- update.sh
+++ update.sh
@@ -7,4 +7,3 @@ NODE_RED_VERSION=$1
 
 sed -i "" -e "s/\(version\": \"\).*\(\"\)/\1$NODE_RED_VERSION\"/g" package.json
 sed -i "" -e "s/\(node-red\": \"\).*\(\"\)/\1$NODE_RED_VERSION\"/g" package.json
-sed -i "" -e "s/\(VERSION=\).*/\1$NODE_RED_VERSION/g" .travis.yml
