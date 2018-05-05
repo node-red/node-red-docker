183e3e295ed027b01976b38885d0e1a9e281a7b7
raymondmouthaan <raymondmmouthaan@gmail.com>
2018-05-05 17:55:00 +0200

    - separate node v6 and v8 functions added

--- .docker/docker.sh
+++ .docker/docker.sh
@@ -17,7 +17,7 @@ main() {
             docker_push
             ;;
         "manifest-list")
-            docker_manifest-list
+            docker_manifest_list
             ;;
         *)
             echo "none of above!"
@@ -26,55 +26,87 @@ main() {
 
 docker_prepare() {
     # Prepare the machine before any code installation scripts
-    ./.travis/main.sh
+    setup_dependencies
 
-    # Prepare qemu
-    docker run --rm --privileged multiarch/qemu-user-static:register --reset
-    mkdir tmp
-    pushd tmp &&
-    curl -L -o qemu-x86_64-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-x86_64-static.tar.gz && tar xzf qemu-x86_64-static.tar.gz &&
-    curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-arm-static.tar.gz && tar xzf qemu-arm-static.tar.gz &&
-    curl -L -o qemu-aarch64-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-aarch64-static.tar.gz && tar xzf qemu-aarch64-static.tar.gz &&
-    popd
+    # Update docker configuration to enable docker manifest command
+    update_docker_configuration
+
+    # Prepare qemu to build images other then x86_64 on travis
+    prepare_qemu
 }
 
 docker_build() {
     echo "DOCKER BUILD: Build all docker images."
-    # node v6 images
-    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=amd64   --build-arg NODE_IMAGE_TAG=6-slim   --build-arg QEMU_ARCH=x86_64  --file ./.docker/Dockerfile.template-debian --tag $IMAGE:latest-v6-linux-amd64 .
-    # docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=arm32v6 --build-arg NODE_IMAGE_TAG=6-alpine --build-arg QEMU_ARCH=arm     --file ./.docker/Dockerfile.template-alpine --tag $IMAGE:latest-v6-linux-arm32v6 .
-    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=arm32v7 --build-arg NODE_IMAGE_TAG=6-slim   --build-arg QEMU_ARCH=arm     --file ./.docker/Dockerfile.template-debian --tag $IMAGE:latest-v6-linux-arm32v7 .
-    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=arm64v8 --build-arg NODE_IMAGE_TAG=6-slim   --build-arg QEMU_ARCH=aarch64 --file ./.docker/Dockerfile.template-debian --tag $IMAGE:latest-v6-linux-arm64v8 .
+    docker_build_node_v6
+    docker_build_node_v8
+}
 
-    # node v8 images
-    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=amd64   --build-arg NODE_IMAGE_TAG=8-slim   --build-arg QEMU_ARCH=x86_64 --file ./.docker/Dockerfile.template-debian --tag $IMAGE:latest-v8-linux-amd64 .
-    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=arm32v6 --build-arg NODE_IMAGE_TAG=8-alpine --build-arg QEMU_ARCH=arm    --file ./.docker/Dockerfile.template-alpine --tag $IMAGE:latest-v8-linux-arm32v6 .
-    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=arm32v7 --build-arg NODE_IMAGE_TAG=8-slim   --build-arg QEMU_ARCH=arm    --file ./.docker/Dockerfile.template-debian --tag $IMAGE:latest-v8-linux-arm32v7 .
-    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=arm64v8 --build-arg NODE_IMAGE_TAG=8-slim   --build-arg QEMU_ARCH=aarch64 --file ./.docker/Dockerfile.template-debian --tag $IMAGE:latest-v8-linux-arm64v8 .
+docker_build_node_v6() {
+    # Build node v6 based images
+    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=amd64   --build-arg NODE_IMAGE_TAG=6-slim   --build-arg QEMU_ARCH=x86_64  --file ./.docker/Dockerfile.template-debian --tag $IMAGE:new-v6-linux-amd64 .
+    # note: there is no node v6 based image available for arm32v6
+    # docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=arm32v6 --build-arg NODE_IMAGE_TAG=6-alpine --build-arg QEMU_ARCH=arm     --file ./.docker/Dockerfile.template-alpine --tag $IMAGE:new-v6-linux-arm32v6 .
+    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=arm32v7 --build-arg NODE_IMAGE_TAG=6-slim   --build-arg QEMU_ARCH=arm     --file ./.docker/Dockerfile.template-debian --tag $IMAGE:new-v6-linux-arm32v7 .
+    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=arm64v8 --build-arg NODE_IMAGE_TAG=6-slim   --build-arg QEMU_ARCH=aarch64 --file ./.docker/Dockerfile.template-debian --tag $IMAGE:new-v6-linux-arm64v8 .
+}
+
+docker_build_node_v8() {
+    # Build node v8 based images
+    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=amd64   --build-arg NODE_IMAGE_TAG=8-slim   --build-arg QEMU_ARCH=x86_64 --file ./.docker/Dockerfile.template-debian --tag $IMAGE:new-v8-linux-amd64 .
+    # note: there is no node v8 slim based image available for arm32v6
+    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=arm32v6 --build-arg NODE_IMAGE_TAG=8-alpine --build-arg QEMU_ARCH=arm    --file ./.docker/Dockerfile.template-alpine --tag $IMAGE:new-v8-linux-arm32v6 .
+    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=arm32v7 --build-arg NODE_IMAGE_TAG=8-slim   --build-arg QEMU_ARCH=arm    --file ./.docker/Dockerfile.template-debian --tag $IMAGE:new-v8-linux-arm32v7 .
+    docker build --no-cache --build-arg NODE_RED_VERSION=v$NODE_RED_VERSION --build-arg ARCH=arm64v8 --build-arg NODE_IMAGE_TAG=8-slim   --build-arg QEMU_ARCH=aarch64 --file ./.docker/Dockerfile.template-debian --tag $IMAGE:new-v8-linux-arm64v8 .
 }
 
 docker_tag() {
     echo "DOCKER TAG: Tag all docker images."
-    # Tag node v6 images
-    docker tag $IMAGE:latest-v6-linux-amd64 $IMAGE:$NODE_RED_VERSION-v6-linux-amd64
-    #docker tag $IMAGE:latest-v6-linux-arm32v6 $IMAGE:$NODE_RED_VERSION-v6-linux-arm32v6
-    docker tag $IMAGE:latest-v6-linux-arm32v7 $IMAGE:$NODE_RED_VERSION-v6-linux-arm32v7
-    docker tag $IMAGE:latest-v6-linux-arm64v8 $IMAGE:$NODE_RED_VERSION-v6-linux-arm64v8
+    docker_tag_node_v6
+    docker_tag_node_v8
+}
 
-    # Tag node v8 images
-    docker tag $IMAGE:latest-v8-linux-amd64 $IMAGE:$NODE_RED_VERSION-v8-linux-amd64
-    docker tag $IMAGE:latest-v8-linux-arm32v6 $IMAGE:$NODE_RED_VERSION-v8-linux-arm32v6
-    docker tag $IMAGE:latest-v8-linux-arm32v7 $IMAGE:$NODE_RED_VERSION-v8-linux-arm32v7
-    docker tag $IMAGE:latest-v8-linux-arm64v8 $IMAGE:$NODE_RED_VERSION-v8-linux-arm64v8
-    echo "DOCKER TAG: Done! All docker images are tagged."
+docker_tag_node_v6() {
+    # Tag node v6 based images
+    docker tag $IMAGE:new-v6-linux-amd64 $IMAGE:latest-v6-linux-amd64
+    docker tag $IMAGE:new-v6-linux-amd64 $IMAGE:$NODE_RED_VERSION-v6-linux-amd64
+
+    #docker tag $IMAGE:latest-v6-linux-arm32v6 $IMAGE:latest-v6-linux-arm32v6
+    #docker tag $IMAGE:latest-v6-linux-arm32v6 $IMAGE:$NODE_RED_VERSION-v6-linux-arm32v6
+
+    docker tag $IMAGE:new-v6-linux-arm32v7 $IMAGE:latest-v6-linux-arm32v7
+    docker tag $IMAGE:new-v6-linux-arm32v7 $IMAGE:$NODE_RED_VERSION-v6-linux-arm32v7
+
+    docker tag $IMAGE:new-v6-linux-arm64v8 $IMAGE:latest-v6-linux-arm64v8
+    docker tag $IMAGE:new-v6-linux-arm64v8 $IMAGE:$NODE_RED_VERSION-v6-linux-arm64v8
+}
+
+docker_tag_node_v8() {
+    # Tag node v8 based images
+    docker tag $IMAGE:new-v8-linux-amd64 $IMAGE:latest-v8-linux-amd64
+    docker tag $IMAGE:new-v8-linux-amd64 $IMAGE:$NODE_RED_VERSION-v8-linux-amd64
+
+    docker tag $IMAGE:new-v8-linux-arm32v6 $IMAGE:latest-v8-linux-arm32v6
+    docker tag $IMAGE:new-v8-linux-arm32v6 $IMAGE:$NODE_RED_VERSION-v8-linux-arm32v6
+
+    docker tag $IMAGE:new-v8-linux-arm32v7 $IMAGE:latest-v8-linux-arm32v7
+    docker tag $IMAGE:new-v8-linux-arm32v7 $IMAGE:$NODE_RED_VERSION-v8-linux-arm32v7
+
+    docker tag $IMAGE:new-v8-linux-arm64v8 $IMAGE:latest-v8-linux-arm64v8
+    docker tag $IMAGE:new-v8-linux-arm64v8 $IMAGE:$NODE_RED_VERSION-v8-linux-arm64v8
 }
 
 docker_push() {
     echo "DOCKER PUSH: Push all docker images."
-    # Push node v6 images
+    docker_push_node_v6
+    docker_push_node_v8
+}
+
+docker_push_node_v6() {
+    # Push node v6 based images
     docker push $IMAGE:latest-v6-linux-amd64
     docker push $IMAGE:$NODE_RED_VERSION-v6-linux-amd64
 
+    # note: there is no node v6 based image available for arm32v6
     #docker push $IMAGE:latest-v6-linux-arm32v6
     #docker push $IMAGE:$NODE_RED_VERSION-v6-linux-arm32v6
 
@@ -83,8 +115,10 @@ docker_push() {
 
     docker push $IMAGE:latest-v6-linux-arm64v8
     docker push $IMAGE:$NODE_RED_VERSION-v6-linux-arm64v8
+}
 
-    # Push node v8 images
+docker_push_node_v8() {
+    # Push node v8 based images
     docker push $IMAGE:latest-v8-linux-amd64
     docker push $IMAGE:$NODE_RED_VERSION-v8-linux-amd64
 
@@ -96,12 +130,17 @@ docker_push() {
 
     docker push $IMAGE:latest-v8-linux-arm64v8
     docker push $IMAGE:$NODE_RED_VERSION-v8-linux-arm64v8
-    echo "DOCKER PUSH: Done! All docker images are pushed."
 }
 
-docker_manifest-list() {
+docker_manifest_list() {
     echo "DOCKER MANIFEST: Create and Push docker manifest list."
+    docker_manifest_list_node_v6
+    docker_manifest_list_node_v8
+    docker_manifest_list_default
+    docker_manifest_list_latest
+}
 
+docker_manifest_list_node_v6() {
     # Manifest Create v6
     docker manifest create $IMAGE:$NODE_RED_VERSION-v6 \
       $IMAGE:$NODE_RED_VERSION-v6-linux-amd64 \
@@ -115,7 +154,9 @@ docker_manifest-list() {
 
     # Manifest Push v6
     docker manifest push $IMAGE:$NODE_RED_VERSION-v6
+}
 
+docker_manifest_list_node_v8() {
     # Manifest Create v8
     docker manifest create $IMAGE:$NODE_RED_VERSION-v8 \
       $IMAGE:$NODE_RED_VERSION-v8-linux-amd64 \
@@ -130,8 +171,10 @@ docker_manifest-list() {
 
     # Manifest Push v8
     docker manifest push $IMAGE:$NODE_RED_VERSION-v8
+}
 
-    # Manifest Create NODE_RED_VERSION
+docker_manifest_list_default() {
+    # Manifest Create NODE_RED_VERSION default
     docker manifest create $IMAGE:$NODE_RED_VERSION \
       $IMAGE:$NODE_RED_VERSION-v8-linux-amd64 \
       $IMAGE:$NODE_RED_VERSION-v8-linux-arm32v6 \
@@ -145,7 +188,9 @@ docker_manifest-list() {
 
     # Manifest Push NODE_RED_VERSION
     docker manifest push $IMAGE:$NODE_RED_VERSION
+}
 
+docker_manifest_list_latest() {
     # Manifest Create LATEST
     docker manifest create $IMAGE:latest \
       $IMAGE:latest-v8-linux-amd64 \
@@ -160,7 +205,51 @@ docker_manifest-list() {
 
     # Manifest Push LATEST
     docker manifest push $IMAGE:latest
-    echo "DOCKER MANIFEST: Create and Push docker manifest list."
+}
+
+setup_dependencies() {
+  echo "PREPARE: Setting up dependencies."
+
+  sudo apt update -y
+  sudo apt install realpath python python-pip -y
+  sudo apt install --only-upgrade docker-ce -y
+  sudo pip install docker-compose || true
+
+  docker info
+  docker-compose --version
+}
+
+update_docker_configuration() {
+  echo "PREPARE: Updating docker configuration"
+
+  mkdir $HOME/.docker
+
+  # enable experimental to use docker manifest command
+  echo '{
+    "experimental": "enabled"
+  }' | tee $HOME/.docker/config.json
+
+  # enable experimental
+  echo '{
+    "experimental": true,
+    "storage-driver": "overlay2",
+    "max-concurrent-downloads": 50,
+    "max-concurrent-uploads": 50
+  }' | sudo tee /etc/docker/daemon.json
+
+  sudo service docker restart
+}
+
+prepare_qemu(){
+    echo "PREPARE: Qemu"
+    # Prepare qemu to build non amd64 / x86_64 images
+    docker run --rm --privileged multiarch/qemu-user-static:register --reset
+    mkdir tmp
+    pushd tmp &&
+    curl -L -o qemu-x86_64-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-x86_64-static.tar.gz && tar xzf qemu-x86_64-static.tar.gz &&
+    curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-arm-static.tar.gz && tar xzf qemu-arm-static.tar.gz &&
+    curl -L -o qemu-aarch64-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-aarch64-static.tar.gz && tar xzf qemu-aarch64-static.tar.gz &&
+    popd
 }
 
 main $1
