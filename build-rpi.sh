5ee05e7e47bcec7a4a3992df3c4ff74de89df391
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2019-02-06 21:52:13 +0100

    Switch rpi base image to arm32v7/node

--- build-rpi.sh
+++ build-rpi.sh
@@ -4,36 +4,43 @@ if [ $# -ne 1 ]; then
 fi
 
 NODE_RED_VERSION=$1
+IMAGE_ORG=${IMAGE_ORG:-nodered}
 
-docker build -f rpi/Dockerfile -t nodered/node-red-docker:rpi .
+# install qemu to build native arm packages
+if [ $(uname) != Darwin ]; then
+    docker run --rm --privileged multiarch/qemu-user-static:register --reset
+fi
+curl -sSL https://github.com/multiarch/qemu-user-static/releases/download/v2.5.0/x86_64_qemu-arm-static.tar.xz | tar -xJ
+
+docker build -f rpi/Dockerfile -t ${IMAGE_ORG}/node-red-docker:rpi .
 
 if [ $? -ne 0 ]; then
     echo "ERROR: Docker build failed for rpi image"
     exit 1
 fi
 
-docker tag nodered/node-red-docker:rpi nodered/node-red-docker:$NODE_RED_VERSION-rpi
+docker tag ${IMAGE_ORG}/node-red-docker:rpi ${IMAGE_ORG}/node-red-docker:$NODE_RED_VERSION-rpi
 
 if [ $? -ne 0 ]; then
     echo "ERROR: Docker tag failed for rpi image"
     exit 1
 fi
 
-docker run -d nodered/node-red-docker:rpi
+docker run -d ${IMAGE_ORG}/node-red-docker:rpi
 
 if [ $? -ne 0 ]; then
     echo "ERROR: Docker container failed to start for rpi build."
     exit 1
 fi
 
-docker push nodered/node-red-docker:rpi
+docker push ${IMAGE_ORG}/node-red-docker:rpi
 
 if [ $? -ne 0 ]; then
     echo "ERROR: Docker push failed for rpi image."
     exit 1
 fi
 
-docker push nodered/node-red-docker:$NODE_RED_VERSION-rpi
+docker push ${IMAGE_ORG}/node-red-docker:$NODE_RED_VERSION-rpi
 
 if [ $? -ne 0 ]; then
     echo "ERROR: Docker push failed for rpi image."
