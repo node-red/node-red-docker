3496e90e4308af39b8dad3fe514d62bd9db6d1fd
raymondmouthaan <raymondmmouthaan@gmail.com>
2019-08-10 09:33:51 +0200

    added replaced inline code to scripts

--- scripts/python_fix.sh
+++ scripts/python_fix.sh
@@ -7,8 +7,8 @@ if [[ ${PYTHON_VERSION} == "3" ]]; then
   sed -i 's/python2.7/python3.6/g' node_modules/\@node-red/nodes/core/hardware/36-rpi-gpio.js
 fi
 
-# Remove Node-Red build-in rpi-gpio for non arm arch
-if ! { [[ ${ARCH} == "arm32v6" ]] || [[ ${ARCH} == "arm32v7" ]]; }; then
+# Remove Node-Red build-in rpi-gpio when python is not installed or for non arm arch
+if [[ ${PYTHON_VERSION} == "0" ]] || ! { [[ ${ARCH} == "arm32v6" ]] || [[ ${ARCH} == "arm32v7" ]]; }; then
   echo "Removing Node-RED build-in rpi-gpio"
   rm -r node_modules/\@node-red/nodes/core/hardware/*
 fi
