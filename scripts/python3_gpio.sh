0ad56fef2adc74e901dd38958d847190b53937cb
raymondmouthaan <raymondmmouthaan@gmail.com>
2019-08-10 09:17:00 +0200

    added replaced inline code to scripts

---
+++ scripts/python3_gpio.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+
+# Install Python3 and pigpio library for arm
+if [[ ${PYTHON_VERSION} == "3" ]]; then
+  echo "Installing Python 3"
+  if [[ ${OS} == "alpine" ]]; then
+    apk add --no-cache python3
+    echo "Fixing Python 3 symlink"
+    ln -s /usr/bin/python3 /usr/bin/python
+    if [[ ${ARCH} == "arm32v6" ]]; then
+      echo "Installing py-rpigpio"
+      apk add py-rpigpio
+    fi
+  else
+    apt-get update && apt-get update && apt-get install -y --no-install-recommends python3
+    echo "Fixing Python 3 symlink"
+    ln -s /usr/bin/python3 /usr/bin/python
+    if [[ ${ARCH} == "arm32v7" ]]; then
+      echo "Installing py-rpigpio"
+      apt-get install -y --no-install-recommends python3-pigpio
+    fi
+  fi
+fi
--- scripts/python_fix.sh
+++ scripts/python_fix.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -ex
 
-if [[ "${PYTHON_VERSION}" == "3" ]] && [[ "${ARCH}" == "arm32v7" ]]; then
+# Fix 36-rpi-gpio for Python3
+if [[ ${PYTHON_VERSION} == "3" ]]; then
   echo "Fixing 36-rpi-gpio.js for Python 3.6"
   sed -i 's/python2.7/python3.6/g' node_modules/\@node-red/nodes/core/hardware/36-rpi-gpio.js
 fi
 
-if [[ "${ARCH}" != "arm32v7" ]]; then
+# Remove Node-Red build-in rpi-gpio for non arm arch
+if ! { [[ ${ARCH} == "arm32v6" ]] || [[ ${ARCH} == "arm32v7" ]]; }; then
   echo "Removing Node-RED build-in rpi-gpio"
   rm -r node_modules/\@node-red/nodes/core/hardware/*
 fi
