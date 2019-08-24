b4c6939b9df56f04f7ca8a2dc4772d26aff4e06d
raymondmouthaan <raymondmmouthaan@gmail.com>
2019-08-24 08:45:39 +0200

    combine functions for easier maintenance.

--- .docker/scripts/install_python_gpio.sh
+++ .docker/scripts/install_python_gpio.sh
@@ -20,16 +20,5 @@ if [[ ${PYTHON_VERSION} != "0" ]]; then
   if [[ ${ARCH} == "arm32v6" ]] || [[ ${ARCH} == "arm32v7" ]]; then
     echo "Installing py-rpigpio"
     apk add py-rpigpio
-
-    echo "Fixing 36-rpi-gpio.js for Python"
-    sed -i 's/python2.7/python/g' node_modules/\@node-red/nodes/core/hardware/36-rpi-gpio.js
-    # Create symlink for python libs
-    ln -s /usr/lib/python$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))') /usr/lib/python
   fi
-fi
-
-# Remove Node-Red build-in rpi-gpio when python is not installed or for non arm arch
-if [[ ${PYTHON_VERSION} == "0" ]] || ! { [[ ${ARCH} == "arm32v6" ]] || [[ ${ARCH} == "arm32v7" ]]; }; then
-  echo "Removing Node-RED build-in rpi-gpio"
-  rm -r node_modules/\@node-red/nodes/core/hardware/*
 fi
