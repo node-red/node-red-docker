d1e120518262ad08d4bc134572a32c602ad3f5ec
Dave Conway-Jones <conway@uk.ibm.com>
2019-12-18 23:27:59 +0000

    Fix healthcheck to read settings.js

--- .docker/healthcheck.js
+++ .docker/healthcheck.js
@@ -1,46 +1,31 @@
 var http = require('http');
 var https = require('https');
+var settings = require('/data/settings.js');
 
 var options = {
-    host : "bf3a15f6.ngrok.io",
-    port : "443",
-    timeout : 10000
+    host : "localhost",
+    port : settings.uiPort || 1880,
+    timeout : 4000
 };
 
-var fail = false;
-
-var request = http.request(options, (res) => {
-    //console.log(`STATUS: ${res.statusCode}`);
-    if (res.statusCode == 200) {
-        process.exit(0);
-    }
-    else {
-        fail = true;
-    }
-});
-
-var requests = https.request(options, (res) => {
-    //console.log(`STATUSS: ${res.statusCode}`);
-    if (res.statusCode == 200) {
-        process.exit(0);
-    }
-    else {
-        fail = true;
-    }
-});
+if (settings.hasOwnProperty("https")) {
+    var requests = https.request(options, (res) => {
+        //console.log(`STATUS: ${res.statusCode}`);
+        if (res.statusCode == 200) { process.exit(0); }
+        else { process.exit(1); }
+    });
+}
+else {
+    var request = http.request(options, (res) => {
+        //console.log(`STATUS: ${res.statusCode}`);
+        if (res.statusCode == 200) { process.exit(0); }
+        else { process.exit(1); }
+    });
+}
 
 request.on('error', function(err) {
     //console.log('ERROR',err);
+    process.exit(1);
 });
-
-requests.on('error', function(err) {
-    //console.log('ERRORS',err);
-});
-
-setTimeout(function() {
-    if (fail) { process.exit(1); }
-    else { process.exit(0); }  // should never get here :-)
-},5000)
 
 request.end(); 
-requests.end(); 
