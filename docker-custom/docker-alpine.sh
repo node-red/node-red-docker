ff4b42684fcdefa9942b34f22291b58875e85d4f
Dave Conway-Jones <conway@uk.ibm.com>
2020-09-14 13:57:21 +0100

    Add default flow with warning for when no Volume mounted.
    Tidy up scripts to make more consistent, and fix readme to match.

--- docker-custom/docker-alpine.sh
+++ docker-custom/docker-alpine.sh
@@ -13,4 +13,4 @@ docker build --rm --no-cache \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
     --build-arg TAG_SUFFIX=default \
     --file Dockerfile.custom \
-    --tag alpine:node-red-build .
+    --tag testing:node-red-build .
--- docker-custom/docker-debian.sh
+++ docker-custom/docker-debian.sh
@@ -13,4 +13,4 @@ docker build --rm --no-cache \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
     --build-arg TAG_SUFFIX=default \
     --file Dockerfile.debian \
-    --tag debian:node-red-build .
+    --tag testing:node-red-build .
