65cf2c955947138cccee0ead8198ff89d83c60f6
Ben Hardill <ben@flowforge.com>
2021-10-15 15:17:30 +0100

    Fix minimal builds
    fixes #277

--- docker-custom/scripts/install_devtools.sh
+++ docker-custom/scripts/install_devtools.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # Installing Devtools
-if [[ ${TAG_SUFFIX} != "minimal" ]]; then
+if [[ ${TAG_SUFFIX} != "-minimal" ]]; then
   echo "Installing devtools"
   apk add --no-cache --virtual devtools build-base linux-headers udev python python3
 else
