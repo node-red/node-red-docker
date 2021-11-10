613a9080ea5903dbc4c919ce2bb2633189d8a7aa
Ben Hardill <ben@flowforge.com>
2021-11-10 10:08:00 +0000

    Fix python package name on Apline (#288)
    Also move .docker/scripts to symlinks to docker-custom/scripst so
    we only have to edit code once

--- .docker/scripts/install_devtools.sh
+++ .docker/scripts/install_devtools.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-set -ex
-
-# Installing Devtools
-if [[ ${TAG_SUFFIX} != "-minimal" ]]; then
-  echo "Installing devtools"
-  apk add --no-cache --virtual devtools build-base linux-headers udev python python3
-else
-  echo "Skip installing devtools"
-fi
+../../docker-custom/scripts/install_devtools.sh
