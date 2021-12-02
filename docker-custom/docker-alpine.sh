b90ff5f4b264ac022b6c71e5f802e59cb39376a4
Ben Hardill <ben@flowforge.com>
2021-12-02 14:02:54 +0000

    Fix base image to be based on Alpine 3.12 (#297)
    This is a fix for
    https://wiki.alpinelinux.org/wiki/Release_Notes_for_Alpine_3.13.0#time64_requirements
    
    fixes #296

--- docker-custom/docker-alpine.sh
+++ docker-custom/docker-alpine.sh
@@ -9,7 +9,7 @@ docker build --rm --no-cache \
     --build-arg ARCH=amd64 \
     --build-arg NODE_VERSION=14 \
     --build-arg NODE_RED_VERSION=${NODE_RED_VERSION} \
-    --build-arg OS=alpine \
+    --build-arg OS=alpine3.12 \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
     --build-arg TAG_SUFFIX=default \
     --file Dockerfile.custom \
