f68f4202f8f5497768d1c75e05486d775cd4d1fe
Ben Hardill <hardillb@gmail.com>
2022-07-16 20:05:48 +0100

    Add PubkeyAccessKeyTypes settings

--- docker-custom/scripts/install_devtools.sh
+++ docker-custom/scripts/install_devtools.sh
@@ -4,7 +4,7 @@ set -ex
 # Installing Devtools
 if [[ ${TAG_SUFFIX} != *"minimal" ]]; then
   echo "Installing devtools"
-  apk add --no-cache --virtual devtools build-base linux-headers udev python2 python3
+  apk add --no-cache --virtual devtools build-base linux-headers udev python3
 else
   echo "Skip installing devtools"
 fi
