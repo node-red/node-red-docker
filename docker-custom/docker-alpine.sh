188056f7312d8cfc9d57649e130897378b24cb10
shyamvish <svishwakarma@3clogic.com>
2024-03-20 14:37:36 +0530

    nodejs version

--- docker-custom/docker-alpine.sh
+++ docker-custom/docker-alpine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export NODE_RED_VERSION=$(grep -oE "\"node-red\": \"(\w*.\w*.\w*.\w*.\w*.)" package.json | cut -d\" -f4)
+export NODE_RED_VERSION=$(grep -oE "\"3c-node-red\": \"(\w*.\w*.\w*.\w*.\w*.)" package.json | cut -d\" -f4)
 
 echo "#########################################################################"
 echo "node-red version: ${NODE_RED_VERSION}"
@@ -7,7 +7,7 @@ echo "#########################################################################"
 
 docker build --rm --no-cache \
     --build-arg ARCH=amd64 \
-    --build-arg NODE_VERSION=16 \
+    --build-arg NODE_VERSION=20 \
     --build-arg NODE_RED_VERSION=${NODE_RED_VERSION} \
     --build-arg OS=alpine \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
